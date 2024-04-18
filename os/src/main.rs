5802a492eba044f44cf72c1ed392d67a23d5ddae
Liquidzk <492928615@qq.com>
2024-04-18 15:04:07 +0800

    kernel

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,6 +1,32 @@
 #![no_std]
 #![no_main]
-mod lang_items;
+#![feature(panic_info_message)]
 
 use core::arch::global_asm;
+
+
+
+#[macro_use]
+mod lang_items;
+mod sbi;
+mod console;
+
 global_asm!(include_str!("entry.asm"));
+
+fn clear_bss() {
+    extern "C" {
+        fn ebss();
+        fn sbss();
+    }
+    (sbss as usize..ebss as usize).for_each(|x| {
+        unsafe {
+            (x as *mut u8).write_volatile(0)
+        }}
+    );
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    panic!("shutdown");
+}
